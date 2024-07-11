198137503ef015273327ae8fc2a619fa33dfc017
César Augusto Vaz Sampaio Filho <62805432+cesarvaz-filho@users.noreply.github.com>
2024-07-11 14:05:46 -0300

    Delete src directory

--- src/routes/helpResponseRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance } from 'fastify';
-import { createHelpResponse, getHelpResponse } from '../controllers/helpResponseController';
-
-async function helpResponseRoutes(fastify: FastifyInstance) {
-  fastify.post('/responses', createHelpResponse);
-  fastify.get('/responses', getHelpResponse);
-}
-
-export default helpResponseRoutes;
