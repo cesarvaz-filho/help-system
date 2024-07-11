1bcb7ab08b3e41b051e3cc58f3874f5d699af117
César Augusto Vaz Sampaio Filho <62805432+cesarvaz-filho@users.noreply.github.com>
2024-07-11 12:18:06 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,16 @@ import Fastify from 'fastify';
 import userRoutes from './routes/userRoutes';
 import helpRequestRoutes from './routes/helpRequestRoutes';
 import helpResponseRoutes from './routes/helpResponseRoutes';
+import cors from 'fastify-cors';
 
 const server = Fastify();
 
+
+server.register(cors, {
+  origin: '*', // Permitir todas as origens
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+});
 server.register(userRoutes);
 server.register(helpRequestRoutes);
 server.register(helpResponseRoutes);
